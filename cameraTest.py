782167841d17c80c349bf23b9b890193c7cce189
Alexey <31387726+Calliska@users.noreply.github.com>
2022-11-11 01:10:30 +0300

    Update cameraTest.py

--- cameraTest.py
+++ cameraTest.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import sys
 import cv2
-# import time
 import os
 import numpy as np
 from cameraFunctions import *
@@ -14,9 +13,9 @@ L = 293
 
 
 def main(argv):
-    #capture from camera at location 0
+    # Устанавливаем настройки камеры
     cmd = "v4l2-ctl --set-ctrl=auto_exposure={0} --set-ctrl=exposure_time_absolute={1} --set-ctrl=brightness={2} " \
-      "--set-ctrl=iso_sensitivity=1 "
+      "--set-ctrl=iso_sensitivity=1 " 
     cmd = cmd.format(ac.autoExposure, ac.exposureTime, ac.brightness)
     os.system(cmd)
     cv2.namedWindow("input")
@@ -33,15 +32,14 @@ def main(argv):
     print("Height: ", height)
     print("Width: ", width)
     M = None
+    
+    # Калибровка по угловым меткам поля для пересчёта в мировые координаты
     for i in range(100):
         ret, img = cap.read()
         isResult, M = calcPerspectiveMatrix(img)
         if isResult:
             print(M)
             break
-    # if len(M) <= 0:
-    #     print("Can`t find corner markers!")
-    #     return 0
     
     img_number = 0
 
@@ -50,63 +48,35 @@ def main(argv):
         ret, img = cap.read()
         showImg = img.copy()
         arucoImg = img.copy()
-        # print("capTime:{}".format(time() - time1))
-        # time1 = time()
         
-        corners, ids, arucoImg = find_markers(img, arucoImg, ac.showFrame)
-        # print("markersTime:{}".format(time() - time1))
-        # time1 = time()
+        corners, ids, arucoImg = find_markers(img, arucoImg, ac.showFrame) 
 
-        balls, showImg = find_ball(img, corners, ac.showFrame)
-        # print("ballTime:{}".format(time() - time1))
-        # time1 = time()
+        balls, showImg = find_ball(img, corners, ac.showFrame) 
 
         ballAll = []
         cornersAll = []
+        # Пересчёт координат для aruco меток
         for corner in corners:
             cornerResult = []
             for i in range(4):
                 cornerPts = np.float32([corner[0][i]])
                 cornerPts = np.array([cornerPts])
-            # print(corner[0][0])
-            # print(corner[0][0][0], corner[0][0][1])
-            # corner[0][0][0] = cv2.perspectiveTransform(corner[0][0][0], M)
                 cornerResult.append(cv2.perspectiveTransform(cornerPts, M))
-            # print(corner, cornerResult)
             cornersAll.append(cornerResult)
-
+        
+        # Пересчёт координат для мяча
         for ball in balls:
             ballPts = np.float32([ball])
             ballPts = np.array([ballPts])
 
             ballResult = cv2.perspectiveTransform(ballPts, M)
-            # ballResult[1] = 283 - ballResult[1]
             y = ballResult[0][0][1]
-            # print(y)
-            # if y < (L/2 - D/2):
-                # w = 1
-            # elif y > (L/2 + D/2):
-                # w = 0
-            # else:
-                # if ac.camId == "1":
-                    # w = 1 - ((y - (L/2 - D/2))/D)
-                # else:
-                    # w = (y - (L/2 - D/2))/D
-            # ballResult[0][0][1] = ballResult[0][0][1]*w
-            # ballResult[0][0][1] = 
-            # print(ballResult)
             ballAll.append(ballResult[0][0])
-        # print(calcArucoCenter(cv2.perspectiveTransform(ball, M)))
-        # print(list(ballAll))
-        # print(cornersAll) 
-        # print(cornersAll[0][0][0][0][0])
-        # print(cornersAll[0][0][0][0][1])
-        # print(cornersAll[0][1][0][0][0])
-        # print(cornersAll[0][1][0][0][1])
+
         sendMQTTData(cornersAll, ids)
         if len(ballAll) <= 1:
             sendBallData(ballAll)
-        # print("Total time: {}".format(time() - time1))
+
         cv2.imshow("input", showImg)
         key = cv2.waitKey(1)
         if key == 27:
